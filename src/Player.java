0f119cb63e2532f74282ec892b1f6dace2b16008
Ruan Santos <96189348+windsarie@users.noreply.github.com>
2022-09-05 23:18:26 -0300

    Update Player.java

--- src/Player.java
+++ src/Player.java
@@ -6,12 +6,15 @@ import javazoom.jl.player.FactoryRegistry;
 import support.PlayerWindow;
 import support.Song;
 
-import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList; // adcionado
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Player {
     /**
@@ -30,53 +33,203 @@ public class Player {
     private PlayerWindow window;
 
     private final ArrayList<Song> songToPlay = new ArrayList<>();
+    private final ReentrantLock lock = new ReentrantLock();
+    private final Condition condition = lock.newCondition();
+    private boolean stop = false;
+    private boolean playerPaused = true;
+
+
+    private Thread threadPlayer;
 
     private int currentFrame = 0;
 
-
-
-    private final ActionListener buttonListenerPlayNow = e -> {};
-    private final ActionListener buttonListenerRemove = e -> {};
-    private final ActionListener buttonListenerAddSong = e -> {
-        ////////////////////////////////////////////////
+    private Song currentSong;
+    private Object await;
+    private final Runnable playerRunnable = () -> {
+        String id = window.getSelectedSong();
+        // procurar no array musica com id
         try {
-            Song newSong = window.openFileChooser();
-            songToPlay.add(newSong);
-            window.setQueueList(getDisplayInfo());
-
-        } catch (IOException | BitstreamException | UnsupportedTagException | InvalidDataException dq) {
-            System.out.println("Erro");
+            this.device = FactoryRegistry.systemRegistry().createAudioDevice();
+        } catch (JavaLayerException e) {
+            throw new RuntimeException(e);
         }
-        ////////////////////////////////////////////////
+        try {
+            this.device.open(this.decoder = new Decoder());
+        } catch (JavaLayerException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            this.bitstream = new Bitstream(currentSong.getBufferedInputStream());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        stop = false;
+        // loop
+        for (int i = 0; i < tamanholista; i++); {
+            if (boolean stop = true){
+                break();
+            } else{
+                await();
+            }
+        }
+        // verificar se o boolean de parar mudou, se sim break
+        // verificar se o boolean de pausar mudou, se sim await
+        playNextFrame
+        // atualizar tempo na janela
+        int frame;
+        int msPerFrame;
+        int numFrames;
+        EventQueue.invokeAndWait(() -> window.setTime(frame * msPerFrame, numFrames * msPerFrame));
 
     };
-    private final ActionListener buttonListenerPlayPause = e -> {};
-    private final ActionListener buttonListenerStop = e -> {};
-    private final ActionListener buttonListenerNext = e -> {};
-    private final ActionListener buttonListenerPrevious = e -> {};
-    private final ActionListener buttonListenerShuffle = e -> {};
-    private final ActionListener buttonListenerLoop = e -> {};
-    private final MouseInputAdapter scrubberMouseInputAdapter = new MouseInputAdapter() {
-        @Override
-        public void mouseReleased(MouseEvent e) {
-        }
 
-        @Override
-        public void mousePressed(MouseEvent e) {
-        }
-
-        @Override
-        public void mouseDragged(MouseEvent e) {
-        }
+    private final ActionListener buttonListenerPlayNow = e -> {
+        threadPlayer = new Thread(playerRunnable);
     };
+    private final ActionListener buttonListenerRemove = e -> {
+        // remover do array
+        // verificar se a musica esta tocando. se sim, parar reproducao
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock.lock();
+
+                    int tamanholista = songToPlay.size();
+
+                    for (int i = 0; i < tamanholista; i++) {
+                        if (filePath.equals(songToPlay.get(i).getFilePath())) {
+
+                            if (indiceMusicaAtual == i) {
+                                indiceMusicaAtual -= 1;
+                                next();
+                                if (indiceMusicaAtual == songToPlay.size() - 1) { //Impedir que ele pule um indice assim que uma música é removida
+                                    stop();
+                                }
+                            }
+                            if (i < indiceMusicaAtual) {
+                                indiceMusicaAtual -= 1;
+                                if (indiceMusicaAtual == 0) {
+                                    window.setEnabledPreviousButton(false);
+                                }
+
+                            }
+                        }
+
+                        songToPlay.remove(i);
+
+                        break;
+                    }
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        private final ActionListener buttonListenerAddSong = e -> {
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        lock.lock();
+                        Song newSong = window.openFileChooser();
+
+                        class HelloRunnable implements Runnable {
+
+                            public void run() {
+                                System.out.println("Hello from a thread!");
+                            }
+
+                            public static void main(String[] args) {
+                                (new Thread(new HelloRunnable())).start();
+                            }
+                        }
+                        songToPlay.add(newSong);
+                        EventQueue.invokeLater
+                        String[][] info = getDisplayInfo();
+                        window.setQueueList(getDisplayInfo());
+                    } catch (IOException | BitstreamException | UnsupportedTagException | InvalidDataException dq) {
+                        System.out.println("Erro");
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }).start();
+        };
+
+        private final ActionListener buttonListenerPlayPause = e -> {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    lock.lock();
+                    try {
+
+                        for (int i = 0; i < songToPlay.size(); i++) {
+                            if (musicStart.equals(songToPlay.get(i).getFilePath())) {
+                                indiceMusicaAtual = i;
+                                threadPlayMusic = new Thread(new RunnablePlay());
+                                playerPaused = false;
+                                window.updatePlayPauseButtonIcon(playerPaused);
+                                window.setEnabledPlayPauseButton(!playerPaused);
+                                threadPlayMusic.start();
+
+                            }
+
+                        }
+                        playerPaused = true;
+                        window.updatePlayPauseButtonIcon(playerPaused);
+
+                    } finally {
+                        lock.unlock();
+                    }
+
+                }
+            }).start();
+
+        }
+
+    private void run() {
+        try {
+            lock.lock();
+            condition.signalAll();
+
+
+        } finally {
+            lock.unlock();
+        }
+        // mudar variavel
+        stop = true
+
+    }
+
+    ;
+    private final ActionListener buttonListenerStop = e -> {
+        // mudar variavel
+        stop = true;
+        //EventQueue.invokeAndWait(() -> window.resetMiniPlayer());
+    };
+
+    //private final ActionListener buttonListenerNext = e -> {};
+    //private final ActionListener buttonListenerPrevious = e -> {};
+    //private final ActionListener buttonListenerShuffle = e -> {};
+    //private final ActionListener buttonListenerLoop = e -> {};
+    //private final MouseInputAdapter scrubberMouseInputAdapter = new MouseInputAdapter() {
+    //@Override
+    //public void mouseReleased(MouseEvent e) {}
+
+    //@Override
+    //public void mousePressed(MouseEvent e) {}
+
+    //@Override
+    //public void mouseDragged(MouseEvent e) {}
+    //};
 
     String windowTitle = "JPlayer";
 
     public Player() {
         EventQueue.invokeLater(() -> window = new PlayerWindow(
-                windowTitle,
-                buttonListenerAddSong
-                //////////////////////////////
+                        windowTitle,
+                        buttonListenerAddSong
+                        //////////////////////////////
                 /*
                 getDisplayInfo(),
                 buttonListenerPlayNow,
@@ -98,57 +251,63 @@ public class Player {
     /**
      * @return False if there are no more frames to play.
      */
-    private boolean playNextFrame() throws JavaLayerException {
+    private String[][] playNextFrame() throws JavaLayerException {
         // TODO Is this thread safe?
-        if (device != null) {
+        lock.lock();
+        try {
+            if (device != null) {
+                Header h = bitstream.readFrame();
+                if (h == null) return false;
+
+                SampleBuffer output = (SampleBuffer) decoder.decodeFrame(h, bitstream);
+                device.write(output.getBuffer(), 0, output.getBufferLength());
+                bitstream.closeFrame();
+            }
+        } finally {
+            lock.unlock();
+        }
+        return true;
+
+
+        /**
+         * @return False if there are no more frames to skip.
+         */
+        private boolean skipNextFrame () throws BitstreamException {
+            // TODO Is this thread safe?
             Header h = bitstream.readFrame();
             if (h == null) return false;
-
-            SampleBuffer output = (SampleBuffer) decoder.decodeFrame(h, bitstream);
-            device.write(output.getBuffer(), 0, output.getBufferLength());
             bitstream.closeFrame();
-        }
-        return true;
-    }
-
-    /**
-     * @return False if there are no more frames to skip.
-     */
-    private boolean skipNextFrame() throws BitstreamException {
-        // TODO Is this thread safe?
-        Header h = bitstream.readFrame();
-        if (h == null) return false;
-        bitstream.closeFrame();
-        currentFrame++;
-        return true;
-    }
-
-    /**
-     * Skips bitstream to the target frame if the new frame is higher than the current one.
-     *
-     * @param newFrame Frame to skip to.
-     * @throws BitstreamException Generic Bitstream exception.
-     */
-    private void skipToFrame(int newFrame) throws BitstreamException {
-        // TODO Is this thread safe?
-        if (newFrame > currentFrame) {
-            int framesToSkip = newFrame - currentFrame;
-            boolean condition = true;
-            while (framesToSkip-- > 0 && condition) condition = skipNextFrame();
-        }
-    }
-    //</editor-fold>
-
-    ///////////////////////////////////////////////////////////
-    private String[][] getDisplayInfo() {
-        String[][] arrayAuxiliar = new String[songToPlay.size()][];
-
-        for (int i=0; i < songToPlay.size(); i++) {
-            arrayAuxiliar[i] = songToPlay.get(i).getDisplayInfo();
+            currentFrame++;
+            return true;
         }
 
-        return arrayAuxiliar;
-    }
-    ///////////////////////////////////////////////////////////
+        /**
+         * Skips bitstream to the target frame if the new frame is higher than the current one.
+         *
+         * @param newFrame Frame to skip to.
+         * @throws BitstreamException Generic Bitstream exception.
+         */
+        private void skipToFrame ( int newFrame) throws BitstreamException {
+            // TODO Is this thread safe?
+            if (newFrame > currentFrame) {
+                int framesToSkip = newFrame - currentFrame;
+                boolean condition = true;
+                while (framesToSkip-- > 0 && condition) condition = skipNextFrame();
+            }
+        }
+        //</editor-fold>
 
+        ///////////////////////////////////////////////////////////
+        private String[][] getDisplayInfo () {
+            String[][] arrayAuxiliar = new String[songToPlay.size()][];
+
+            for (int i = 0; i < songToPlay.size(); i++) {
+                arrayAuxiliar[i] = songToPlay.get(i).getDisplayInfo();
+            }
+
+            return arrayAuxiliar;
+        }
+        ///////////////////////////////////////////////////////////
+
+    }
 }
